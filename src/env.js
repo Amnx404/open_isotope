ac198d429ea49e7c468f3825dbdf800c6b9a5852
Aman <91665648+Amnx404@users.noreply.github.com>
2025-03-20 17:51:14 +0530

    Revert "chore: add various configuration and utility files to node_modules"
    This reverts commit e5d2f50108b4870ff82af6880629af58ffb99865.

--- src/env.js
+++ src/env.js
@@ -17,10 +17,6 @@ export const env = createEnv({
       .default("development"),
     GOOGLE_CLIENT_ID: z.string(),
     GOOGLE_CLIENT_SECRET: z.string(),
-    PAYU_MERCHANT_ID: z.string(),
-    PAYU_MERCHANT_KEY: z.string(),
-    PAYU_BASE_URL: z.string().url(),
-    NEXTAUTH_URL: z.string().url(),
   },
 
   /**
@@ -42,10 +38,6 @@ export const env = createEnv({
     NODE_ENV: process.env.NODE_ENV,
     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
     GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
-    PAYU_MERCHANT_ID: process.env.PAYU_MERCHANT_ID,
-    PAYU_MERCHANT_KEY: process.env.PAYU_MERCHANT_KEY,
-    PAYU_BASE_URL: process.env.PAYU_BASE_URL,
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
